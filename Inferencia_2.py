18ced3214fd5a7fbf14f7891dc605effc6bd98e9
GABY210992 <gabyestef21@gmail.com>
2025-05-31 11:47:05 -0500

    Inferencia_2.py

--- Inferencia_2.py
+++ Inferencia_2.py
@@ -1,9 +1,9 @@
 import joblib
 
 # Datos fijos para prueba
-features = [[2000, 500000, 20,5, 1500, 100, 4,'Diesel','Individual','Manual','Second Owner' ]]
+features = [[2000, 500000, 5]]
 # Cargar el modelo
-modelo = joblib.load("modelo_regresion.pkl")
+modelo = joblib.load("modelo_regresion1.pkl")
 # Hacer la predicción
 prediccion = modelo.predict(features)
 print("Predicción:", prediccion[0])
