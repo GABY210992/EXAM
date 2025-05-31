fcece85addef2489f1409c48b88b9b2d1065d13b
GABY210992 <gabyestef21@gmail.com>
2025-05-31 12:32:51 -0500

    main.py

--- main.py
+++ main.py
@@ -3,43 +3,18 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import joblib
 import numpy as np
-import pandas as pd
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Carga tu modelo
-modelo_regresion = joblib.load("modelo_regresion1.pkl")
+model = joblib.load("modelo_regresion1.pkl")
 
 @app.get("/", response_class=HTMLResponse)
 def form_get(request: Request):
-    # Mostrar formulario vacío
     return templates.TemplateResponse("form.html", {"request": request})
 
 @app.post("/predict", response_class=HTMLResponse)
-def predict(
-    request: Request,
-    year: int = Form(...),
-    km_driven: int = Form(...),
-    seats: int = Form(...)
-):
-    # Crear DataFrame con los datos recibidos
-    nuevo_caso = pd.DataFrame({
-        'year': [year],
-        'km_driven': [km_driven],
-        'seats': [seats]
-    })
-
-    # Predecir usando el modelo cargado
-    prediccion = modelo_regresion.predict(nuevo_caso)[0]
-
-    # Mostrar resultado en el formulario
-    return templates.TemplateResponse(
-        "form.html",
-        {
-            "request": request,
-            "result": f"El valor estimado es: {prediccion:.2f}"
-        }
-    )
-
+def predict(request: Request, year: float = Form(...), km_driven: float = Form(...), seats: float = Form(...)]])
+    prediction = model.predict(features)[0]
+    return templates.TemplateResponse("form.html", {"request": request, "result": f"Predicción: Clase {prediction}"})
 
